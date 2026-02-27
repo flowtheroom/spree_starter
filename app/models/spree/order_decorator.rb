c2935500fcd6773ddfa90e7aca22b304de20ee9e
Cynthia Chan <cynthia.chan@flowtheroom.com>
2026-02-27 16:04:01 +0800

    fix: Spree::Order.order_refunded?, Spree::Order.partially_refunded?

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -7,6 +7,20 @@ module Spree
         super
       end
     end
+
+    def order_refunded?
+      return false if item_count.zero?
+
+      (payment_state.in?(%w[void failed]) && refunds_total.positive?) ||
+        refunds_total >= total_minus_store_credits
+    end
+
+    def partially_refunded?
+      return false if item_count.zero?
+      return false if payment_state.in?(%w[void failed]) || refunds.empty?
+
+      refunds_total < total_minus_store_credits
+    end
   end
 
   Spree::Order.prepend(OrderDecorator)
